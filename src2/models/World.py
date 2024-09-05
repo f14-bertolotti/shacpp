16d395a8c46a76279576c07ce7046039e645e9a7
Francesco Bertolotti <f14.bertolotti@gmail.com>
2024-09-05 13:50:04 +0200

    added world models

--- src2/models/World.py
+++ src2/models/World.py
@@ -1,12 +1,15 @@
 import torch
 
 class World(torch.nn.Module):
-    def __init__(self, observation_size, action_size, agents, layers=3, hidden_size=128, heads=2, feedforward_size=512, dropout=0.1, max_positions=512, activation="relu", device="cuda:0"):
+    def __init__(self, observation_size, action_size, agents, steps, layers=3, hidden_size=128, heads=2, feedforward_size=512, dropout=0.1, max_positions=512, activation="relu", device="cuda:0"):
         super().__init__()
 
         self.agents = agents
 
-        self.hidden_first = torch.nn.Linear((observation_size+action_size)*agents, hidden_size, device = device)
+        self.obs2hid = torch.nn.Linear(observation_size, hidden_size, device = device)
+        self.act2hid = torch.nn.Linear(action_size     , hidden_size, device = device)
+        self.actpos = torch.nn.Parameter(torch.empty(1, steps, 1, hidden_size, device = device).normal_(0,0.02))
+        self.agnpos = torch.nn.Parameter(torch.empty(1, 1, agents, hidden_size, device = device).normal_(0,0.02))
 
         self.encoder = torch.nn.TransformerEncoder(
             torch.nn.TransformerEncoderLayer(
@@ -20,15 +23,20 @@ class World(torch.nn.Module):
             num_layers = layers
         )
 
-        self.hidden_last = torch.nn.Linear(hidden_size, observation_size*agents, device = device)
+        self.hid2obs = torch.nn.Linear(hidden_size, observation_size, device = device)
 
-    def step(self, obs, act):
-        oba = torch.cat([obs, act], dim=-1)
-        hidden = self.hidden_first(oba.flatten(1,2))
-        for act, drp, nrm, lin in zip(self.hidden_acts, self.hidden_drops, self.hidden_norms, self.hidden_lins):
-            hidden = nrm(hidden + drp(act(lin(hidden))))
+    def forward(self, obs, act):
+        act = act.transpose(0,1) # ENVS x STEPS x AGENTS x ACTSIZE
+        obs = obs.unsqueeze(1)   # ENVS x 1     x AGENTS x OBSSIZE
 
-        prd_obs = self.hidden_last(hidden).view(*obs.shape)
-        return prd_obs + obs
+        hidobs = self.obs2hid(obs)
+        hidact = self.act2hid(act) + self.actpos
+
+        hidden = torch.cat([hidobs, hidact], dim=1) + self.agnpos
+
+        encoded = self.encoder(hidden.flatten(1,2)).view(hidden.shape)
+
+        obs = self.hid2obs(encoded) + obs
+        return obs[:,:-1,:,:].transpose(0,1)
 
 
