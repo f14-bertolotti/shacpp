f44269644fc103345e2f80af5264f2f95fefd36c
Francesco Bertolotti <f14.bertolotti@gmail.com>
2024-11-01 14:40:23 +0100

    policy/reward/world eval mode in evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -12,6 +12,10 @@ def evaluate(
         reward_model = None,
         world_model = None
     ):
+    policy_model.eval()
+    if reward_model: reward_model.eval()
+    if world_model :  world_model.eval()
+
     eval_episode = unroll(
         observations = None, 
         world        = world,
@@ -27,12 +31,16 @@ def evaluate(
         proxy = world_model(eval_episode["observations"][0].unsqueeze(1), eval_episode["actions"][:world_model.steps].transpose(0,1))[0].transpose(0,1)
         rewards = rewards[:world_model.steps]
 
+    policy_model.train()
+    if reward_model: reward_model.train()
+    if world_model :  world_model.train()
+
     logger.info(json.dumps({
         "episode"            : episode,
         "done"               : eval_episode["dones"][-1,:,0].sum().int().item(),
         "reward"             : rewards.sum().item() / envs,
     } | ({} if proxy is None else {
-        "reward_acc" : proxy.isclose(rewards, atol=.1).float().mean().item(),
+        "reward_acc"         : proxy.isclose(rewards, atol=.1).float().mean().item(),
     })))
 
     return {
