038e889ea982b56d4aa796631ed3767b78847043
Francesco Bertolotti <f14.bertolotti@gmail.com>
2024-11-04 21:26:39 +0100

    added TransformerReward model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from models.Model  import Model
 from models.Policy import PolicyAFO, PolicyOFA
 from models.Reward import RewardAFO, RewardOFA
+from models.TransformerReward import TransformerReward
 from models.Value  import ValueAFO ,  ValueOFA
 from models import worlds
