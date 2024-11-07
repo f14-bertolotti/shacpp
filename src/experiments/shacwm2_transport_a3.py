316a85a055f12e0289e95d9de4b8ec43dfa21180
Francesco Bertolotti <f14.bertolotti@gmail.com>
2024-11-07 22:37:04 +0100

    better shacwm2 conf

--- src/experiments/shacwm2_transport_a3.py
+++ src/experiments/shacwm2_transport_a3.py
@@ -10,12 +10,22 @@ import os
 def run():
 
     config = experiments.configs.shacwm2
-    config.dir              = "data/shacwm2-transport-a3"
+    config.dir              = "data/shacwm2-transport-a3-2"
     config.observation_size = 11
     config.action_size      = 2
     config.agents           = 3
 
     config.eval_steps = 512
+    config.episodes = 100000
+    config.value_cache_size = 10000
+    config.value_bins = 10
+    config.etv = 100
+    config.world_ett = 10
+    config.reward_ett = 10
+    config.value_ett = 10
+    config.value_stop_threshold = None
+    config.reward_stop_threshold = None
+    config.world_stop_threshold = None
 
     os.makedirs(config.dir, exist_ok=False)
     utils.save_config(config.dir, config.__dict__)
